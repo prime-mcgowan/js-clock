7dfd7eced6205f4b5912a2e7c7d90139c8ea48d6
Laura McGowan <prime.lmcg@gmail.com>
2024-03-29 12:43:02 -0500

    updated clock with central standard time

--- js/app.js
+++ js/app.js
@@ -7,19 +7,23 @@ function setDate() {
   const now = new Date();
 
   // * get seconds *********************************************
-  const seconds = now.getSeconds(); //gets the seconds of every minute
+  const cstOffset = -6; // CST if UTC - 6
+  const cstTime = new Date(now.getTime() + cstOffset * 60 * 60 * 1000);
+
+  // * get seconds *********************************************
+  const seconds = cstTime.getSeconds(); //gets the seconds of every minute
   //   console.log(seconds);
   const secondsDegrees = (seconds / 60) * 360 + 90; //moves the clock hand degrees
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   //   console.log(seconds);
 
   // * get minutes *********************************************
-  const minutes = now.getMinutes();
+  const minutes = cstTime.getMinutes();
   const minutesDegrees = (minutes / 60) * 360 + 90;
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   // * get hour *********************************************
-  const hour = now.getMinutes();
+  const hour = cstTime.getMinutes();
   const hourDegrees = (hour / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
